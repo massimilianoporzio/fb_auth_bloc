48a6e7174349a0bc1551c5db57c50358e600f493
MASSIMILIANO PORZIO <massimiliano.porzio@gmail.com>
2023-02-19 17:39:08 +0100

    Home

--- lib/core/presentation/pages/home_page.dart
+++ lib/core/presentation/pages/home_page.dart
@@ -1,5 +1,8 @@
 import 'package:fb_auth_bloc/core/routes/app_routes.dart';
+import 'package:fb_auth_bloc/features/auth/presentation/blocs/auth/auth_bloc.dart';
+import 'package:fb_auth_bloc/features/auth/presentation/pages/profile_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
   static const String routeName = AppRoutes.home;
@@ -14,8 +17,43 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: const Scaffold(
-        body: Center(child: Text('Home')),
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false, //non faccio vedere l'icon leading
+          //questo perché non torno indietro dalla home
+          title: const Text('Home'),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  //NAVIGO AL PROFILO
+                  Navigator.pushNamed(context, ProfilePage.routeName);
+                },
+                icon: const Icon(Icons.account_circle)),
+            IconButton(
+                onPressed: () {
+                  //CHIAMO IL BLOC E PASSO l'EVENTO SIGNOUT
+                  context.read<AuthBloc>().add(SignoutRequestedEvent());
+                },
+                icon: const Icon(Icons.logout))
+          ],
+        ),
+        body: Center(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/bloc_logo_full.png',
+              width: MediaQuery.of(context).size.width * 0.8,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+                'Bloc is an awesome\nstate management libray\nfor flutter!',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24))
+          ],
+        )),
       ),
     );
   }
